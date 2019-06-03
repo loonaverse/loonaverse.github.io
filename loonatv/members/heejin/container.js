92741cd5ba05c81d3f7c8979b1fa72f9fdebcd94
unnamedfreak <>
2019-06-03 19:12:24 +0800

    [190603] Content is now divided into files for easier modification
    - Episodes are now divided into individual JSONs instead of one index.html. (Phew! Easier to maintain!)
    - JSON data is added via `container.js`
    - Currently testing on /heejin only.

---
+++ loonatv/members/heejin/container.js
@@ -0,0 +1,38 @@
+$(document).ready(
+    function() {
+
+        for(i=1; i<=18; i++) {
+            let api = `epFiles/ep${i}.json`;
+            var xmlhttp = new XMLHttpRequest();
+            let container = document.getElementById("container");
+
+
+            xmlhttp.onreadystatechange = function() {
+                if(this.readyState == 4 && this.status == 200) {
+                    var myObj = JSON.parse(this.responseText);
+
+                    let body = `<div class="content" id="ep${myObj.id}">   
+                            <div class="youtube" data-embed="j1JamHYWJfY"><div class="play-button"></div></div>
+                            <div class="summary">
+                                <b><u>Episode:</u> ${myObj.title}</b>
+                                <br><b><u>Date:</u></b> ${myObj.date}
+                                <br><u>Era:</u> HeeJin Solo
+                                <br><u>Members:</u> ${myObj.members}
+                                <br><u>Blurred Girls:</u> ${myObj.blurred}
+                                <br><u>Songs:</u> ${myObj.songs}
+                                <br><u>Location:</u> ${myObj.location}
+                                <br><u>Synopsis/Memes:</u> 
+                                <br> ${myObj.summary}
+                            </div>
+                        </div>`
+                    $('#container').append(body);
+
+                        // container.innerHTML = body;
+                }
+            };
+
+            xmlhttp.open("GET", api, true);
+            xmlhttp.send();
+        }
+    }
+);
