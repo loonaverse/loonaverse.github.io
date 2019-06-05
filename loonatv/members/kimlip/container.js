892ea512dff858299a7bed687654b9e8e91aafd1
unnamedfreak <32760644+unnamedfreak@users.noreply.github.com>
2019-06-05 13:52:19 +0800

    [190605] This should work...

--- loonatv/members/kimlip/container.js
+++ loonatv/members/kimlip/container.js
@@ -1,4 +1,4 @@
-(
+var promise1 = new Promise(
     function() {
         console.log("Container loaded!")
         for(i=123; i<=142; i++) {
@@ -36,3 +36,32 @@
         }
     }
 );
+
+promise1.then( function() {
+    console.log("JS loaded!");
+    var youtube = document.querySelectorAll( ".youtube" );
+    
+    for (var i = 0; i < youtube.length; i++) {
+        
+        var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
+        
+        var image = new Image();
+                image.src = source;
+                image.addEventListener( "load", function() {
+                    youtube[ i ].appendChild( image );
+                }( i ) );
+        
+                youtube[i].addEventListener( "click", function() {
+
+                    var iframe = document.createElement( "iframe" );
+
+                            iframe.setAttribute( "frameborder", "0" );
+                            iframe.setAttribute( "allowfullscreen", "" );
+                            iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+
+                            this.innerHTML = "";
+                            this.appendChild( iframe );
+                } );    
+    };
+    
+} )();
