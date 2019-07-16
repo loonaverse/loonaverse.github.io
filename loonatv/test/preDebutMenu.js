62c1666f7ea49b5d384ef6c35b860b1902138b03
unnamedfreak <>
2019-07-17 01:22:24 +0800

    [190516] LTV Pre-Debut Page Finished

--- loonatv/test/preDebutMenu.js
+++ loonatv/test/preDebutMenu.js
@@ -1,10 +1,8 @@
-(function() {
-    
-    let preMenu = `
-        
-    `
-
-
-    $('#preDebut').click(() => {     
+$(document).ready(
+    function() {
+        console.log('works');
+    $('#preDebut').click(() => {
+        $('.menu').css('display', 'none');
+        $('.preContainer').css('display', 'block');
     });
 })();
